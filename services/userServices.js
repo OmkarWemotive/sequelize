53f372c764453c0b3f70b94b9f6f16c59c75cd53
gatitaa-omkar <omkar@gatitaa.in>
2021-03-30 16:17:57 +0530

    serach user service added

--- services/userServices.js
+++ services/userServices.js
@@ -3,6 +3,8 @@ const db=require('../models')
 const Users =db.users
 const FriendRequest=db.friendRequest
 
+//---------------------------------Save user---------------------------------------------
+
 const saveUser = async(data)=> {
     try
     {
@@ -11,10 +13,30 @@ const saveUser = async(data)=> {
     }
     catch(e)
     {
+        throw Error('Error while saving users')
+    }
+}
+//---------------------------------Search user---------------------------------------------
+const searchUser = async(userName)=>{
+    try
+    {
+            const data=await Users.findAll({
+                attributes:['id','name','email'],
+                where:
+                {
+                    name:{[Op.like]:userName+"%" }
+                },
+            })
+            res.status(200).json(data)
+    }
+    catch(e)
+    {
         res.status(400).send(e)
     }
 }
 
+
 module.exports={
-    saveUser
+    saveUser,
+    searchUser
 }
